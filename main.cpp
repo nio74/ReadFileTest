db12cd77ff65c03679ed9202997a8063dd9281e1
nio74 <mpaolo74@gmail.com>
2020-11-16 20:27:18 +0100

    init

--- main.cpp
+++ main.cpp
@@ -1,23 +1,5 @@
-// basic file operations
-/*#include <iostream>
-#include <fstream>
-using namespace std;
-
-int main () {
-  ofstream myfile;
-  myfile.open ("file.txt");
-  myfile << "Writing this to a file.\n";
-  myfile.close();
-  return 0;
-}*/
-
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include "ReadFile.h"
 
-using namespace std;
 
 int main()
 { 
